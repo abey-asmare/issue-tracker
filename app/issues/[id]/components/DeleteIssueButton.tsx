e90d94dac424281d47af60180b97a413b667e391
abey asmare <abeyasmare07@gmail.com>
2025-06-20 23:06:32 +0300

    Show a confirmation dialog box

--- app/issues/[id]/components/DeleteIssueButton.tsx
+++ app/issues/[id]/components/DeleteIssueButton.tsx
@@ -1,13 +1,35 @@
 import { TrashIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
+import { Button, AlertDialog, Flex } from "@radix-ui/themes";
 
 function DeleteIssueButton({ issueId }: { issueId: number }) {
   return (
-    <Button color="red">
-      <TrashIcon />
-      <Link href={`/issues/${issueId}/`}>Edit Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <TrashIcon />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you wanted to delete this Content, this can&apos;t be
+          undone
+        </AlertDialog.Description>
+        <Flex mt="4" gap='3'>
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red" >
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 }
 export default DeleteIssueButton;
