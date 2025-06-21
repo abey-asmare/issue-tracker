bf2ac906c62aa10e871999e346d340c401985c05
abey asmare <abeyasmare07@gmail.com>
2025-06-21 21:17:06 +0300

    Refactor: refactor nav links

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -13,14 +13,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 function Nav() {
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
-  ];
-  console.log(session);
   return (
     <nav className="border-b border-zinc-600 px-6 py-2 mb-4">
       <Container>
@@ -29,48 +21,9 @@ function Nav() {
             <Link href="/">
               <AiFillBug className="h-5 w-5" />
             </Link>
-            <ul className="flex items-center gap-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={clsx(" hover:text-zinc-800 transition-colors", {
-                      "text-zinc-900": currentPath === link.href,
-                      "text-zinc-500": currentPath !== link.href,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLink />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    size="2"
-                    radius="full"
-                    src={session.user!.image!}
-                    fallback="?"
-                    className="cursor-pointer hover:opacity-85"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout/">Log out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin/">Log in</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
@@ -78,3 +31,61 @@ function Nav() {
 }
 
 export default Nav;
+function NavLink() {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <ul className="flex items-center gap-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={clsx("nav-link", {
+              "!text-zinc-900": currentPath === link.href,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function AuthStatus() {
+  const { status, data: session } = useSession();
+  if (status === "loading") return null;
+  return (
+    <Box>
+      {status === "authenticated" ? (
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Avatar
+              size="2"
+              radius="full"
+              src={session.user!.image!}
+              fallback="?"
+              className="cursor-pointer hover:opacity-85"
+            />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text size="2">{session.user!.email}</Text>
+            </DropdownMenu.Label>
+            <DropdownMenu.Item>
+              <Link href="/api/auth/signout/">Log out</Link>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      ) : (
+        <Link className="nav-link" href="/api/auth/signin/">
+          Log in
+        </Link>
+      )}
+    </Box>
+  );
+}
