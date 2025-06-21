861353ae06f24be1e381bc9c20c1f8d8e59c2923
abey asmare <abeyasmare07@gmail.com>
2025-06-21 20:48:58 +0300

    Add a dropdown menu to show the current user

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -1,5 +1,12 @@
 "use client";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 import clsx from "clsx";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -13,11 +20,12 @@ function Nav() {
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+  console.log(session);
   return (
-    <nav className="border-b border-zinc-600 px-6 py-4 mb-4">
+    <nav className="border-b border-zinc-600 px-6 py-2 mb-4">
       <Container>
         <Flex justify="between" align="center">
-          <Flex gap='3'>
+          <Flex gap="3">
             <Link href="/">
               <AiFillBug className="h-5 w-5" />
             </Link>
@@ -39,7 +47,25 @@ function Nav() {
           </Flex>
           <Box>
             {status === "authenticated" && (
-              <Link href="/api/auth/signout/">Log out</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    size="2"
+                    radius="full"
+                    src={session.user!.image!}
+                    fallback="?"
+                    className="cursor-pointer hover:opacity-85"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout/">Log out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             )}
             {status === "unauthenticated" && (
               <Link href="/api/auth/signin/">Log in</Link>
